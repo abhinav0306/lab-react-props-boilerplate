49800528123c970455be8b1394266a9071019b00
abhinav0306 <abhinavs0603@gmail.com>
2024-01-06 16:06:34 +0530

    App.jsx complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
 import './App.css';
-import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-let data=imageData()
+
 
 let galStyle={
   display:"grid",
   gridTemplateColumns:"repeat(2,1fr)",
 }
-function App() {
+function App(props) {
   // code here
+  let data =props.props
   return(
     <>
     <h1 style={{textAlign:"center",fontSize:"50px"}}>Kalvium Gallary(Functional Component)</h1>
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AppClass from "./AppClass";
-
+import data from './Data';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />   
+    <App props={data} />   
     {/* change the above line - for interchaning between App and AppClass */}
-    <AppClass/>
+    {/* <AppClass/> */}
   </React.StrictMode>
 );
+
